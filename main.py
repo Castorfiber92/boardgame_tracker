b0874bfa3fa99efd862e20b340c18bcf33696c5d
Karl Tengström <karl.tengstrom@gmail.com>
2026-05-11 21:00:45 +0200

    Add homepage with session list

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
-from tracker.storage.database import Base, engine
+# storage folder
+from tracker.storage.database import Base, engine, get_session
+
+# models folder
 from tracker.models.player import Player
 from tracker.models.boardgame import BoardGame
 from tracker.models.session import Session
 from tracker.models.result import GameResult
 
+# api folder
 from tracker.api.players import router as players_router
 from tracker.api.boardgames import router as boardgames_router
 from tracker.api.sessions import router as sessions_router
 
-from fastapi import FastAPI
+# fastapi imports
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+from sqlalchemy.orm import joinedload
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
 app.include_router(players_router)
 app.include_router(boardgames_router)
 app.include_router(sessions_router)
-@app.get("/")
-def root():
-    return {"message": "Boardgame Tracker API"}
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    with get_session() as db:
+        sessions = db.query(Session).options(
+            joinedload(Session.boardgame),
+            joinedload(Session.results).joinedload(GameResult.player)
+        ).all()
+    return templates.TemplateResponse(
+    request=request,
+    name="index.html",
+    context={"sessions": sessions}
+)
