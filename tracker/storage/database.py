f235c0d0352b7d5c8008b88593c59da3abccd19b
Karl Tengström <karl.tengstrom@gmail.com>
2026-05-12 12:54:30 +0200

    Add PostgreSQL support via enviroment variable

--- tracker/storage/database.py
+++ tracker/storage/database.py
@@ -1,10 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session
 
 class Base(DeclarativeBase):
     pass
 
-engine = create_engine("sqlite:///boardgame_tracker.db", echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///boardgame_tracker.db")
+
+engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
     return Session(engine)
